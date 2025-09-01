a612c17098f49893b9a34fdea2a558d2b421b52c
Kunal_senslyze <kunal.joshi@senslyze.com>
2025-09-01 12:29:26 +0530

    feat(monitoring): add SigNoz metrics instrumentation for observability
    Added OpenTelemetry-based SigNoz integration to track application performance,
    traces, and logs for improved monitoring and debugging.

---
+++ apps/web/src/instrumentation.ts
@@ -0,0 +1,15 @@
+import { registerOTel, OTLPHttpJsonTraceExporter } from "@vercel/otel";
+// Add otel logging
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR); // set diaglog level to DEBUG when debugging
+export function register() {
+  registerOTel({
+    serviceName: "email_assitant_frontend",
+    traceExporter: new OTLPHttpJsonTraceExporter({
+      url: "https://ingest.in.signoz.cloud:443/v1/traces",
+      headers: {
+        "signoz-ingestion-key": "4409af11-c954-48a1-a67e-b1e8ef23ce03",
+      },
+    }),
+  });
+}
